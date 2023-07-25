3a1752b463ad4c5725de65fbb16e1c3493325410
Tahmid Hasan <67729687+IamTahmidHasan14@users.noreply.github.com>
2023-07-25 19:42:08 +0600

    Add files via upload

---
+++ Product/src/product/Product.java
@@ -0,0 +1,13 @@
+package product;
+public class Product {
+
+    public static void main(String[] args) {
+        String productName;
+        int unitSold;
+        float unitPrice;
+        
+    }
+    private static void ProductPrice() {
+        
+    }   
+}
